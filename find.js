5c92f0e90b56c452de4ff30fb3e82031f219f9fc
MrEricL <eli6@stuy.edu>
2017-12-12 14:05:25 -0500

    changes

--- find.js
+++ find.js
@@ -13,12 +13,22 @@ console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-  /* YOUR IMPLEMENTATION */
+  var dis = Math.sqrt(Math.pow((x1-x0),2)+Math.pow((y1-y0),2));
+  return dis;
 };
 
 
 var findIt = function(e) {
   /* YOUR IMPLEMENTATION */
+  var xpos = e.clientX;
+  var ypos = e.clientY;
+  var dis = distance(targetX,targetY,xpos,ypos);
+  console.log(dis)
+  if (dis==0){
+  	box.style.backgroundColor="#FF0000";
+  }
+  
+
 };
 
 /*
